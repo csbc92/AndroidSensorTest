e3905222f4a3779002a86b42997bdab9bb20e10e
Christian Clausen <csbc92@hotmail.com>
2019-09-12 11:37:17 +0200

    GPS getLastLocation is working

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,21 +3,42 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    // Logic to handle location object
+                    TextView lat = findViewById(R.id.txtLatitude);
+                    TextView longt = findViewById(R.id.txtLongitude);
+                    lat.setText(String.valueOf(location.getLatitude()));
+                    longt.setText(String.valueOf(location.getLongitude()));
+                }
+            }
+        });
+
     }
 
     public void sendMessage(View view) {
@@ -29,6 +50,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendLocation(View view) {
-        GoogleApiAvailability.getInstance();
+
     }
 }
